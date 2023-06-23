f579f1916ead2ae4705f472da08477ba05426685
fithamlak <justiceofgod01@gmail.com>
2023-06-23 22:38:01 +0300

    define routes and fic rubocop

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,32 +1,27 @@
-module API
-    module V1
-      class UsersController < ApplicationController
-        def index
-          @users = User.all
-          render json: @users
-        end
-  
-        def create
-          @user = User.new(user_params)
-  
-          if @user.save
-            render json: @user, status: :created
-          else
-            render json: @user.errors, status: :unprocessable_entity
-          end
-        end
-  
-        def show
-          @user = User.find(params[:id])
-          render json: @user
-        end
-  
-        private
-  
-        def user_params
-          params.require(:user).permit(:username)
-        end
-      end
+class Api::V1::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
-  
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+end
