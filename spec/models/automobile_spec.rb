dcdf499d841cb005d331335b8ba1507c714288ae
fithamlak <justiceofgod01@gmail.com>
2023-06-26 21:10:33 +0300

    finish spec on aoutomobile model for all validations

--- spec/models/automobile_spec.rb
+++ spec/models/automobile_spec.rb
@@ -29,4 +29,22 @@ RSpec.describe Automobile, type: :model do
     expect(automobile).not_to be_valid
     expect(automobile.errors[:year]).to include("must be less than or equal to #{Date.today.year}")
   end
+
+  it 'is invalid without a rate' do
+    automobile = Automobile.new(rate: nil)
+    expect(automobile).not_to be_valid
+    expect(automobile.errors[:rate]).to include("can't be blank")
+  end
+
+  it 'is invalid with a non-positive rate' do
+    automobile = Automobile.new(rate: -1000)
+    expect(automobile).not_to be_valid
+    expect(automobile.errors[:rate]).to include("must be greater than 0")
+  end
+
+  it 'is invalid without a location' do
+    automobile = Automobile.new(location: nil)
+    expect(automobile).not_to be_valid
+    expect(automobile.errors[:location]).to include("can't be blank")
+  end
 end  
