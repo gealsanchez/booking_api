b2d36394aa8e33fe3c8cb7cad08dfcc792a99bfb
fithamlak <justiceofgod01@gmail.com>
2023-06-28 02:18:51 +0300

    Committing local changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :automobiles
+      resources :tickets
+      resources :users, only: [:show, :destroy]
+    end
+  end
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
