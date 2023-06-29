18bdf8af57ad4dbad9a6c535edbee2d8845622bd
fithamlak <justiceofgod01@gmail.com>
2023-06-30 02:27:46 +0300

    refactor api controllers for user login

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -1,36 +1,23 @@
 class Api::V1::TicketsController < ApplicationController
-  before_action :set_ticket, only: %i[show destroy]
-
   def index
-    @tickets = current_user.tickets
-    render json: @tickets
+    @user = User.includes(tickets: { automobile: :image_attachment }).find(params[:user_id])
+    @tickets = @user.tickets
+    render json: @tickets.(include: { automobile: { methods: :image_url } })
   end
 
-  def show
-    render json: @ticket
-  end
-
+  # POST /tickets
   def create
-    @ticket = current_user.tickets.build(ticket_params)
+    @ticket = Ticket.new(ticket_params)
+    @user = User.find(params[:user_id])
+    @ticket.user = @user
     if @ticket.save
       render json: @ticket, status: :created
     else
       render json: @ticket.errors, status: :unprocessable_entity
     end
   end
-
-  def destroy
-    @ticket.destroy
-    head :no_content
-  end
-
-  private
-
-  def set_ticket
-    @ticket = Ticket.find(params[:id])
-  end
-
+  # Only allow a list of trusted parameters through.
   def ticket_params
-    params.require(:ticket).permit(:date, :automobile_id)
+    params.require(:ticket).permit(:automobile_id, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :automobiles
-      resources :tickets
-      resources :users, only: [:show, :destroy]
+      resources :users, only: [:index,:create,:show] do
+        resources :tickets, only: [:index, :create]
+      end
+      resources :automobiles, only: [:index, :show, :create, :destroy]
     end
   end
 end
